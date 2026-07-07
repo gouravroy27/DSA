9e4585d3251759d22af8a2242c40c499661817a0
Gourav Roy <gouravroy27062005@gmail.com>
2026-07-08 02:48:49 +0530

    Time: 4 ms (9.67%) | Memory: 50.3 MB (33.72%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -0,0 +1,17 @@
+class Solution{
+    public:
+    int findMaxConsecutiveOnes(vector<int> & nums){
+        int max_count=0;
+        int current_count = 0;
+        for(int num: nums){
+            if(num == 1){
+                current_count++;
+                max_count = std::max(max_count, current_count);
+            }
+            else {
+                current_count = 0;
+            }
+        }
+        return max_count;
+    }
+};
