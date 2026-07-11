fadf191c9f6fd4abc39b1290f043d929f3214ff5
Gourav Roy <gouravroy27062005@gmail.com>
2026-07-11 22:18:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (41.11%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        std::stack<int> st;
+
+        for(const std::string & token : tokens) {
+            if (token == "+" || token == "-" || token == "*" || token == "/") {
+                int rightOperand = st.top();
+                st.pop();
+                int leftOperand = st.top();
+                st.pop();
+        
+                if (token == "+") {
+                    st.push(leftOperand + rightOperand);
+                }
+                else if (token == "-") {
+                    st.push(leftOperand - rightOperand);
+                }
+                else if (token == "*") {
+                    st.push(leftOperand * rightOperand);
+                }
+                else if (token == "/") {
+                    st.push(leftOperand / rightOperand);
+                }
+            }
+            else {
+                st.push(std::stoi(token));
+            }
+        }
+        return st.top();
+    }
+};
